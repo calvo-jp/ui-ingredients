ebb53fda254f5cceeefa958b9d425ea1d3540f5b
jp calvo <calvojp92@gmail.com>
2024-08-18 20:26:54 +0800

    chore: use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
+import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +9,6 @@ const config = {
   },
   compilerOptions: {
     runes: true,
-    css: 'injected',
   },
 };
 
