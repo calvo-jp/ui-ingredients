aa3b72dc2bd3729fbf121667eee2553046f11012
jp calvo <calvojp92@gmail.com>
2024-08-19 17:13:49 +0800

    chore: refactor types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,3 @@
-export type Assign<T, U> = Omit<T, keyof U> & U;
+export type Assign<Target, Source> = Omit<Target, keyof Source> & Source;
 export type OptionalId<T> = Assign<T, {id?: string}>;
 export type WithoutChildren<T> = Omit<T, 'children'>;
