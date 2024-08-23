84438e4f847997e0db8786fc4e3f3fabd369c725
jp calvo <calvojp92@gmail.com>
2024-08-23 12:28:20 +0800

    chore: unexpose HtmlIngredientProps

--- packages/ui-ingredients/src/lib/index.ts
+++ packages/ui-ingredients/src/lib/index.ts
@@ -34,5 +34,3 @@ export * from './timer/index.js';
 export * from './toast/index.js';
 export * from './toggle-group/index.js';
 export * from './tooltip/index.js';
-
-export type {HtmlIngredientProps} from './types.js';
