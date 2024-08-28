342c20ded00448273e60ec67f09013280449e79f
jp calvo <calvojp92@gmail.com>
2024-08-28 21:36:51 +0800

    chore: scaffold website

---
+++ website/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import '@fontsource/inter/400.css';
+  import '@fontsource/inter/500.css';
+  import '@fontsource/inter/600.css';
+  import '@fontsource/inter/700.css';
+  import '../app.css';
+
+  let {children} = $props();
+</script>
+
+{@render children()}
