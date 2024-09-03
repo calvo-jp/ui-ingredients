eb6701cd4a120c9eb42437911512ad98fe308a3f
jp calvo <calvojp92@gmail.com>
2024-09-03 20:49:42 +0800

    fix: classname

--- packages/ui-ingredients/src/routes/collapsible/+page.svelte
+++ packages/ui-ingredients/src/routes/collapsible/+page.svelte
@@ -18,7 +18,7 @@
   </Collapsible.Trigger>
 
   <Collapsible.Content
-    class="data-open:animate-collapse-in lg: data-closed:animate-collapse-out mt-3 w-full max-w-[32rem] overflow-hidden rounded"
+    class="data-open:animate-collapse-in data-closed:animate-collapse-out mt-3 w-full overflow-hidden rounded lg:max-w-[32rem]"
   >
     <div class="rounded border p-4">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus odit
