6de5340b83f8bb3df97ce07030cdecf15a54c0f4
jp calvo <calvojp92@gmail.com>
2024-09-17 16:30:32 +0800

    fix: vitest types

--- packages/ui-ingredients/vitest.d.ts
+++ packages/ui-ingredients/vitest.d.ts
@@ -4,8 +4,9 @@ import type {TestingLibraryMatchers} from '@testing-library/jest-dom/matchers';
 import type {AxeMatchers} from 'vitest-axe/matchers';
 
 declare module 'vitest' {
-  interface Assertion<T> extends TestingLibraryMatchers<T>, AxeMatchers {}
-  interface AsymmetricMatchersContaining<T>
-    extends TestingLibraryMatchers<T>,
+  interface Assertion extends TestingLibraryMatchers<any, any>, AxeMatchers {}
+
+  interface AsymmetricMatchersContaining
+    extends TestingLibraryMatchers<any, any>,
       AxeMatchers {}
 }
