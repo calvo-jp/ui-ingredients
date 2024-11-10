7305dcc1c84266c8419cb6d50ce72bd27a9ccb9f
jp calvo <calvojp92@gmail.com>
2024-11-10 11:47:27 +0800

    init pages

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+  import '@fontsource/fira-code/400.css';
+  import '@fontsource/inter/400.css';
+  import '@fontsource/inter/500.css';
+  import '@fontsource/inter/600.css';
+  import '@fontsource/inter/700.css';
   import '../app.css';
 
   let {children} = $props();
--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -1,11 +1,18 @@
 import aspectRatio from '@tailwindcss/aspect-ratio';
 import typography from '@tailwindcss/typography';
 import type {Config} from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
-export default {
+const config: Config = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      sans: ["'Inter'", ...defaultTheme.fontFamily.sans],
+      mono: ["'Fira Code'", ...defaultTheme.fontFamily.mono],
+    },
     extend: {},
   },
   plugins: [typography, aspectRatio],
-} satisfies Config;
+};
+
+export default config;
