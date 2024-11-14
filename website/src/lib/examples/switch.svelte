68cd7c5e4613583e34ecc7a5581c3b950e4b6857
jp calvo <calvojp92@gmail.com>
2024-11-14 20:47:00 +0800

    fix

--- website/src/lib/examples/switch.svelte
+++ website/src/lib/examples/switch.svelte
@@ -2,13 +2,15 @@
   import {Switch} from 'ui-ingredients';
 </script>
 
-<Switch.Root class="mx-auto w-fit">
-  <Switch.Control
-    class="relative h-10 w-16 rounded-full bg-neutral-800 transition-colors duration-150 data-checked:bg-indigo-500"
-  >
-    <Switch.Thumb
-      class="absolute left-1 top-1 h-8 w-8 rounded-full bg-white shadow-md transition-all duration-200 data-checked:translate-x-[calc(100%-theme(spacing.2))]"
-    />
-  </Switch.Control>
-  <Switch.HiddenInput />
-</Switch.Root>
+<div class="m-0 flex justify-center">
+  <Switch.Root>
+    <Switch.Control
+      class="relative h-10 w-16 rounded-full bg-neutral-800 transition-colors duration-150 data-checked:bg-indigo-500"
+    >
+      <Switch.Thumb
+        class="absolute left-1 top-1 h-8 w-8 rounded-full bg-white shadow-md transition-all duration-200 data-checked:translate-x-[calc(100%-theme(spacing.2))]"
+      />
+    </Switch.Control>
+    <Switch.HiddenInput />
+  </Switch.Root>
+</div>
