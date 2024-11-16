dfb48afdf4b4ba318c75c2152c28357eb627ae97
jp calvo <calvojp92@gmail.com>
2024-11-16 18:14:53 +0800

    fix toast demo

--- website/src/lib/demo/toast.svelte
+++ website/src/lib/demo/toast.svelte
@@ -1,17 +1,39 @@
 <script lang="ts">
   import {Button} from '$lib/ui';
+  import {createToaster, Toast, Toaster} from 'ui-ingredients';
   import Container from './container.svelte';
+
+  let toaster = createToaster({
+    duration: 5000,
+    placement: 'bottom-end',
+    overlap: true,
+  });
 </script>
 
 <Container>
   <Button
     onclick={() => {
-      // toaster.create({
-      //   title: 'Yesss!',
-      //   description: 'This is a toast message',
-      // });
+      toaster.create({
+        title: 'Success!',
+        description: 'This is a toast message',
+      });
     }}
+    class="mx-auto w-auto"
   >
-    Click me
+    Create toast
   </Button>
 </Container>
+
+<Toaster {toaster}>
+  <Toast.Root
+    class="z-[var(--z-index)] h-[var(--height)] min-w-[90vw] scale-[var(--scale)] rounded border border-neutral-800 bg-neutral-900 p-4 opacity-[var(--opacity)] transition-all duration-300 [translate:var(--x)_var(--y)_0] lg:min-w-[20rem]"
+  >
+    <Toast.Title class="font-medium" />
+    <Toast.Description class="text-sm text-neutral-400" />
+    <Toast.CloseTrigger class="mt-3">
+      {#snippet asChild(attrs)}
+        <Button {...attrs} fullWidth variant="outline">Close</Button>
+      {/snippet}
+    </Toast.CloseTrigger>
+  </Toast.Root>
+</Toaster>
