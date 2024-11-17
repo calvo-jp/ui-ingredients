fb8f03036fb8be7465cc3c6e5a60151146384001
jp calvo <calvojp92@gmail.com>
2024-11-17 15:23:43 +0800

    x

--- website/src/lib/demo/tour.svelte
+++ website/src/lib/demo/tour.svelte
@@ -1,49 +1,64 @@
 <script lang="ts">
   import {Button} from '$lib/ui';
   import {XCloseIcon} from '@untitled-theme/icons-svelte';
+  import {twMerge} from 'tailwind-merge';
   import {Tour, type TourStepDetails} from 'ui-ingredients';
   import Container from './container.svelte';
 
-  const length = 5;
-  const steps = Array.from({length}).map((_, idx) => {
-    const n = idx + 1;
+  const length = 3;
+  const steps: TourStepDetails[] = [
+    // {
+    //   type: 'dialog',
+    //   title: 'Welcome',
+    //   description: 'Welcome to the tour!',
+    //   actions: [
+    //     {
+    //       label: 'Lets go!',
+    //       action: 'next',
+    //     },
+    //   ],
+    //   placement: 'center',
+    // },
+    ...Array.from({length}).map((_, idx) => {
+      const n = idx + 1;
 
-    const actions: TourStepDetails['actions'] = [];
+      const actions: TourStepDetails['actions'] = [];
 
-    if (n > 1) {
-      actions.push({
-        label: 'Prev',
-        action: 'prev',
-      });
-    }
+      if (n > 1) {
+        actions.push({
+          label: 'Prev',
+          action: 'prev',
+        });
+      }
 
-    if (n < 5) {
-      actions.push({
-        label: 'Next',
-        action: 'next',
-      });
-    }
+      if (n < length) {
+        actions.push({
+          label: 'Next',
+          action: 'next',
+        });
+      }
 
-    if (n === length) {
-      actions.push({
-        label: 'Done',
-        action: 'dismiss',
-      });
-    }
+      if (n === length) {
+        actions.push({
+          label: 'Done',
+          action: 'dismiss',
+        });
+      }
 
-    const o: TourStepDetails = {
-      type: 'tooltip',
-      arrow: true,
-      title: `Step ${n} Title`,
-      description: `Step ${n} Description`,
-      actions,
-      target() {
-        return document.getElementById(`step-${n}-target`);
-      },
-    };
+      const o: TourStepDetails = {
+        type: 'tooltip',
+        arrow: true,
+        title: `Step ${n} Title`,
+        description: `Step ${n} Description`,
+        actions,
+        target() {
+          return document.getElementById(`step-${n}-target`);
+        },
+      };
 
-    return o;
-  });
+      return o;
+    }),
+  ];
 </script>
 
 <Container>
@@ -53,14 +68,14 @@
 
       <Tour.Trigger>
         {#snippet asChild(attrs)}
-          <Button {...attrs}>Start</Button>
+          <Button variant="outline" {...attrs}>Start</Button>
         {/snippet}
       </Tour.Trigger>
-      <Tour.Backdrop class="bg-black/50 backdrop-blur-sm" />
+      <Tour.Backdrop class="bg-black/75 backdrop-blur-sm" />
       <Tour.Spotlight />
       <Tour.Positioner>
         <Tour.Content
-          class="relative w-[24rem] rounded border bg-neutral-900 p-5"
+          class="relative w-[24rem] rounded border border-neutral-800 bg-neutral-900 p-5"
         >
           <Tour.Arrow
             class="[--arrow-background:theme(colors.neutral[900])] [--arrow-size:theme(spacing.4)]"
@@ -68,9 +83,9 @@
             <Tour.ArrowTip />
           </Tour.Arrow>
 
-          <Tour.ProgressText class="text-muted text-sm" />
+          <Tour.ProgressText class="text-sm text-neutral-400" />
           <Tour.Title class="mt-4 text-lg font-medium" />
-          <Tour.Description class="text-muted" />
+          <Tour.Description class="text-neutral-400" />
 
           <div class="mt-5 flex justify-end gap-3">
             {#each actions as action}
@@ -92,15 +107,33 @@
     {/snippet}
   </Tour.Root>
 
-  <div class="mt-10 grid gap-4 lg:grid-cols-2">
+  <div class="mt-10 grid gap-5 lg:grid-cols-3">
     {#each Array.from({length}) as _, idx}
       {@const n = idx + 1}
 
       <div
         id="step-{n}-target"
-        class="text-muted data-[tour-highlighted]:border-accent rounded border p-5 data-[tour-highlighted]:text-neutral-200"
+        class={twMerge(
+          'grid',
+          'place-items-center',
+          'rounded-md',
+          'aspect-video',
+          'text-3xl',
+          'font-mono',
+          'font-bold',
+          'text-white',
+          'data-[tour-highlighted]:outline',
+          'data-[tour-highlighted]:outline-2',
+          'data-[tour-highlighted]:outline-white',
+          'data-[tour-highlighted]:outline-offset-4',
+          [
+            'bg-gradient-to-r from-rose-700 to-red-700',
+            'bg-gradient-to-r from-amber-700 to-yellow-700',
+            'bg-gradient-to-r from-emerald-700 to-green-700',
+          ][idx],
+        )}
       >
-        Target {n}
+        {n}
       </div>
     {/each}
   </div>
