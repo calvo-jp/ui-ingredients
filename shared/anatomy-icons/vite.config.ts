c875dc9b66b64c7c14cf3c3468fea44ccc367a50
jp calvo <calvojp92@gmail.com>
2024-12-24 20:14:41 +0800

    chore(anatomy-icons): refactor

--- shared/anatomy-icons/vite.config.ts
+++ shared/anatomy-icons/vite.config.ts
@@ -1,9 +1,12 @@
-import {sveltekit} from '@sveltejs/kit/vite';
+import {svelte} from '@sveltejs/vite-plugin-svelte';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  server: {
-    port: 4000,
-  },
+  plugins: [
+    svelte({
+      compilerOptions: {
+        runes: true,
+      },
+    }),
+  ],
 });
