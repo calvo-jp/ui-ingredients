9d503e108f67bac45c228c61eee148f0ef7f8a8d
jp calvo <calvojp92@gmail.com>
2024-12-26 18:37:13 +0800

    chore(website): bump ui-ingredients-plugin-tailwindcss

--- website/src/lib/ui/icon-button.recipe.ts
+++ website/src/lib/ui/icon-button.recipe.ts
@@ -1,11 +1,11 @@
-import { tv, type VariantProps } from 'tailwind-variants';
+import {tv, type VariantProps} from 'tailwind-variants';
 
 export const iconButtonRecipe = tv({
   base: 'size-12 shrink-0 flex items-center justify-center gap-2 rounded font-semibold transition-colors duration-150 disabled:cursor-not-allowed disabled:opacity-50',
   variants: {
     variant: {
       outline:
-        'border border-neutral-200 bg-white dark:bg-transparent dark:border-neutral-700 ui-pressed:bg-neutral-100/75 dark:ui-pressed:bg-neutral-700/25',
+        'border border-neutral-200 bg-white dark:bg-transparent dark:border-neutral-700 ui-pressed:bg-neutral-100/75 ui-on:bg-neutral-100/75 dark:ui-pressed:bg-neutral-700/25 dark:ui-on:bg-neutral-700/25',
       solid: 'bg-neutral-300',
     },
   },
